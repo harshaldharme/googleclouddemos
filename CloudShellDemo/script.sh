bb62f92d269e06154d4caa6dbfca2ca9c375528b
Harshal Dharme <harshalvdharme@gmail.com>
2022-07-20 18:02:39 +0530

    Added sample code for cloud shell demo

---
+++ CloudShellDemo/script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "Listing the projects"
+gcloud projects list
+
+echo ""
+echo ""
+echo "Listing the configurations"
+gcloud config configurations list
+
+echo ""
+echo ""
+echo "Creating a config for dev environment"
+gcloud config configurations create dev-config
+gcloud config set core/account edurekagcpbatch18062022@gmail.com
+gcloud config set core/project centered-flash-353712
+gcloud config set compute/region us-central1
+gcloud config set compute/zone us-central1-a
+
+echo ""
+echo ""
+echo "Listing the configurations"
+gcloud config configurations list
+
+echo ""
+echo ""
+echo "Enabling the compute engine API"
+gcloud services enable compute.googleapis.com
+
+echo ""
+echo ""
+echo "Create a new VM machine"
+gcloud compute instances create edureka-vm1 --zone=us-central1-a  --machine-type=f1-micro --image-project=centos-cloud --image-family=centos-7 --boot-disk-type=pd-standard \
+--boot-disk-size=20GB
+
+echo ""
+echo "Compute engine created successfully"
+
+echo ""
+echo ""
+echo "Printing virtual machines in project"
+gcloud compute instances list
+
+echo "Exiting.."
+exit 0
