c7322b053a9c7b29b014244d4d865acd201c5fab
Harshal Dharme <32070236+harshaldharme@users.noreply.github.com>
2022-11-25 23:54:42 -0800

    Update main.py

--- AppEngineDemo/main.py
+++ AppEngineDemo/main.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return '<body style="background-color: lightblue"><br/><h1> Welcome to Edureka Learning Portal </h1><h4> Course Contents </h4><p></p><p>Introduction to <strong>Google App Engine</strong></p><p></p><p>Your New Instructor: <strong>Riya</strong></p><img src=\'https://clipart.world/wp-content/uploads/2020/08/young-teacher-png.png\'></body>'
+    return '<body style="background-color: lightblue"><br/><h1> Welcome to Edureka Learning Portal </h1><h4> Course Contents </h4><p></p><p>Introduction to <strong>Google App Engine</strong></p><p></p><p>Your New Instructor: <strong>Jennica</strong></p><img src=\'https://freepngimg.com/save/35555-teacher-clipart/512x512\'></body>'
