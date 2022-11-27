278d5e22b036056ebc5f1db72d47d0e858ca7fe3
Harshal Dharme <32070236+harshaldharme@users.noreply.github.com>
2022-11-27 06:15:31 -0800

    Update main.py

--- AppEngineDemo/main.py
+++ AppEngineDemo/main.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return '<body style="background-color: lightblue"><br/><h1> Welcome to Edureka Learning Portal </h1><h4> Course Contents </h4><p></p><p>Introduction to <strong>Google App Engine</strong></p><p></p><p>Your New Instructor: <strong>Jennica</strong></p><img src=\'https://freepngimg.com/save/35555-teacher-clipart/512x512\'></body>'
+    return '<body style="background-color: lightgreen"><br/><h1> Welcome to Edureka Learning Portal </h1><h4> Take a look at Google Cloud Cheatsheet </h4><p></p><p></p><img src=\'https://www.googlecloudcommunity.com/gc/image/serverpage/image-id/41615iF0164B367E605DE8?v=v2\'></body>'
